fcbd2f6a3ac585ce160ac81abdd5116ebdada15a
Ryan Aubrey <rma7qb@virginia.edu>
2020-11-18 17:58:35 -0500

    Update

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -32,8 +32,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Sign up for our thing.. :)</h1>
-        <h1 className={styles.title}>What happens to the PR when I update?</h1>
+        <h1 className={styles.title}>HI! Sign up for our thing.. :)</h1>
 
         <Row>
           <Col xs={24} sm={10} style={{ margin: 10 }}>
