62ed2a38c7d722b1ac60923a860a83c3c51455e2
Ryan Aubrey <rma7qb@virginia.edu>
2020-11-20 11:29:06 -0500

    Another test, deploy functions 3

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,6 @@ import { onCall as onCallFunction } from "firebase-functions/lib/providers/https
 export const helloWorld = onCallFunction(() => {
   const message = `Hello from Firebase at ${new Date().toLocaleString()}!`;
   functions.logger.info("Hello logs!", { message });
-  functions.logger.info("Test 2!");
+  functions.logger.info("Test 3!");
   return message;
 });
