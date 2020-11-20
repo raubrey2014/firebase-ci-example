ebae4fb39916532895e4885321955d507d1d6782
Ryan Aubrey <rma7qb@virginia.edu>
2020-11-20 11:18:29 -0500

    Should deploy only functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,5 +4,6 @@ import { onCall as onCallFunction } from "firebase-functions/lib/providers/https
 export const helloWorld = onCallFunction(() => {
   const message = `Hello from Firebase at ${new Date().toLocaleString()}!`;
   functions.logger.info("Hello logs!", { message });
+  functions.logger.info("Test!");
   return message;
 });
